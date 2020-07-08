747ac5c548313544a65ddd5e228802dcb5639f1d
jinwoo park <swat018@gmail.com>
2020-07-09 08:04:37 +0900

    바이트코드 조작 - javaagent

--- src/main/java/com/swat018/Masulsa.java
+++ src/main/java/com/swat018/Masulsa.java
@@ -2,7 +2,9 @@ package com.swat018;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,8 +13,12 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Masulsa {
     public static void main(String[] args) {
-/*        try {
-            new ByteBuddy().redefine(Moja.class)
+  /*      ClassLoader classLoader = Masulsa.class.getClassLoader();
+        TypePool typePool = TypePool.Default.of(classLoader);
+        try {
+            new ByteBuddy().redefine(
+                    typePool.describe("com.swat018.Moja").resolve(),
+                    ClassFileLocator.ForClassLoader.of(classLoader))
                     .method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
                     .make().saveIn(new File("/Users/jinwoopark/Desktop/git/classloadersample/target/classes/"));
         } catch (IOException e) {
