5999e8a648cd7b588f5a9045baa695c8d5705111
jinwoo park <swat018@gmail.com>
2022-01-25 22:23:02 +0900

    feat : java version 11로 변경.

--- src/main/java/me/swat018/App.java
+++ src/main/java/me/swat018/App.java
@@ -4,10 +4,8 @@ package me.swat018;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
